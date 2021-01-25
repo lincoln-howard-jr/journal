85b4937fc60a20aa7662444967368eb360d0a6fc
Lincoln Howard <lincoln.c.howard.jr@gmail.com>
2021-01-25 11:02:13 -0500

    Updates

--- sw.js
+++ sw.js
@@ -1,37 +1,22 @@
-const shouldCache = (url, method) => {
-  if (method !== 'get') return false;
-  let yes = ['http://localhost:3000', 'https://fonts.gstatic.com', 'https://fonts.googleapis.com', 'https://lincoln-howard-jr.github.io/journal', 'https://akqxdqgf7l.execute-api.us-east-1.amazonaws.com/Prod'].filter (start => url.startsWith (start));
-  console.log (url, method, yes);
-  return yes.length > 0;
-}
 // on install
 self.addEventListener ('install', event => {
-  event.waitUntil (caches.open ('v1').then (cache => {
-  }));
+  event.waitUntil (caches.open ('v1'));
 });
 
 self.addEventListener('activate', event => {
-  console.log ('activated');
+  console.log (self.clients.claim ());
 });
 
-self.addEventListener('fetch', event => {
+self.addEventListener ('fetch', event => {
+  
+  if (event.request.method.toLowerCase () !== 'get') return;
+  
   event.respondWith (
-    caches.open ('v1').then (function (cache) {
-      return cache.match (event.request).then (function (matching) {
-        if (matching) return matching;
-        return fetch (event.request).then (response => {
-          return response;
-        });
-      })
-    })
+    (async () => {
+      let cache = await caches.open ('v1');
+      let match = await cache.match (event.request);
+      event.waitUntil (cache.add (event.request));
+      return !!match ? match : fetch (event.request);
+    }) ()
   );
-
-  event.waitUntil (
-    caches.open ('v1').then (function (cache) {
-      if (!shouldCache (event.request.url, event.request.method)) return;
-      return fetch (event.request).then (function (response) {
-        return cache.put (event.request, response);
-      });
-    })
-  )
 });
