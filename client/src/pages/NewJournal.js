6d1026fc02c46171f8bbd06691fff411a1780ffd
Lincoln Howard <lincoln.c.howard.jr@gmail.com>
2021-08-03 08:07:07 -0400

    fix layout issue, add modal back

--- client/src/pages/NewJournal.js
+++ client/src/pages/NewJournal.js
@@ -1,12 +1,48 @@
 import { useState } from "react";
 import { useApp } from "../AppProvider";
-import { dateShortHand, getTime } from "../lib/indexing";
+import { dateShortHand, getTime, printDate } from "../lib/indexing";
 import { H1, H2, H3 } from "./components/Headers";
 import Query from "./components/Query";
 import AnalogClock from "./components/viz/AnalogClock";
+import TrashSVG from '../img/trash.svg'
+
+function RemoveEntryModal ({entry, resolve, reject}) {
+  const app = useApp ();
+  const handleYes = async () => {
+    try {
+      await app.journal.hideEntry (entry.id);
+      resolve ();
+    } catch (e) {
+      reject ('Couldn\'t  successfully hide entry!')
+    }
+  }
+  const handleNo = () => reject ('Cancelled');
+  return (
+    <div className="grid remove-entry-modal">
+      <p>Are you sure you want to hide your journal entry from {printDate (entry.start)} at {getTime (entry.start)}</p>
+      <div>
+        <button onClick={handleYes}>Yes, hide it!</button>
+        <button onClick={handleNo}>No, don't!</button>
+      </div>
+    </div>
+  )
+}
 
 function JournalCard ({idx}) {
+  const {setModal, closeModal} = useApp ();
   const [open, setOpen] = useState ('');
+  const openRemoveModal = entry => new Promise (async (resolve, reject) => {
+    setModal (
+      <RemoveEntryModal entry={entry} resolve={resolve} reject={reject} />
+    )
+  });
+  const remove = entry => async () => {
+    try {
+      await openRemoveModal (entry);
+    } finally {
+      closeModal ();
+    }
+  }
   return (
     <article className={open}>
       <header onClick={() => {setOpen (open === '' ? 'open' : '')}}>
@@ -24,9 +60,13 @@ function JournalCard ({idx}) {
       {
         idx.list.map (entry => (
           <section>
-            <H3 short={getTime (entry.start)}>
-              {getTime (entry.start)} - {getTime (entry.end)}
-            </H3>
+            <header>
+              <span />
+              <H3 short={getTime (entry.start)}>
+                {getTime (entry.start)} - {getTime (entry.end)}
+              </H3>
+              <span onClick={remove (entry)}><img src={TrashSVG} /></span>
+            </header>
             {
               (entry.entryType === 'questions' || !entry.entryType) &&
               entry.questions.map ((q, i) => (
